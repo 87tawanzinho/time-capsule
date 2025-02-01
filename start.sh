587de42ede6a977880b23648b886d35393f0de84
87tawanzinho <87tawanzinho@gmail.com>
2025-02-01 14:01:39 -0300

    v

--- start.sh
+++ start.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# Rodar o servidor PHP (Laravel)
-php artisan serve --host=0.0.0.0 --port=8000 &
-
-# Rodar o processo de fila do Laravel
-php artisan queue:listen --tries=1 &
-
-# Rodar o Vite (frontend)
-npm run dev &
-
-#!/bin/bash
-
 # Rodar o comando "composer run dev" para iniciar o Laravel e o Vite
 composer run dev
-
-# Manter o container em execução
-wait -n
 
 # Manter o container em execução
 wait -n
