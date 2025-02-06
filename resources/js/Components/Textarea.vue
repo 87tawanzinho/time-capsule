010bc00506b32f5c2dafbadf762bad29a136b5c0
Fellipe Macedo <marciojunior@MacBook-Pro-de-Marcio.local>
2025-02-06 13:32:43 -0300

    ..

--- resources/js/Components/Textarea.vue
+++ resources/js/Components/Textarea.vue
@@ -3,7 +3,7 @@
         <quill-editor
             ref="quillEditor"
             v-model:content="formStore.form.message"
-            contentType="delta"
+            contentType="html"
             theme="snow"
             :toolbar="toolbarOptions"
             class="custom-editor"
